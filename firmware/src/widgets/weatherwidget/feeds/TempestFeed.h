3a1536f007427036e2b1092b1b0e125bf05e5065
David Reed <212698+dreed47@users.noreply.github.com>
2025-03-04 23:11:26 -0500

    added Tempest feed

--- firmware/src/widgets/weatherwidget/feeds/TempestFeed.h
+++ firmware/src/widgets/weatherwidget/feeds/TempestFeed.h
@@ -6,15 +6,23 @@
 #include <ArduinoJson.h>
 #include <TaskManager.h>
 
+#ifndef TEMPEST_PROXY_URL
+    #define TEMPEST_PROXY_URL "https://tempestproxy.infoorbs.com/proxy"
+#endif
 class TempestFeed : public WeatherFeed {
 public:
-    TempestFeed(const String &apiKey, const std::string &location);
+    TempestFeed(const String &apiKey, const String &stationId, int units, const String &stationName);
     bool getWeatherData(WeatherDataModel &model) override;
     void processResponse(int httpCode, const String &response, WeatherDataModel &model);
     void preProcessResponse(int httpCode, String &response);
+    String translateIcon(const std::string &icon);
 
 private:
     String apiKey;
+    String stationId;
+    String stationName;
+    std::string m_proxyUrl = TEMPEST_PROXY_URL;
+    int units;
 
 #ifdef WEATHER_UNITS_METRIC
     int m_weatherUnits = 0;
