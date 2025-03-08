13f4de6758c05980e37d952529a623fe040bfea7
nconner-06 <neil.conner@outlook.com>
2025-03-08 23:34:40 +0100

    Add files via upload

---
+++ firmware/src/widgets/matrixwidget/MatrixTranslations.h
@@ -0,0 +1,17 @@
+#ifndef MATRIXTRANSLATIONS_H
+#define MATRIXTRANSLATIONS_H
+
+#include "I18n.h"
+
+// All translation variables for MatrixWidget should start with "t_matrix"
+
+extern Translation t_matrixBigFont;
+extern Translation t_matrixTextColor;
+extern Translation t_matrixHeadTextColor;
+extern Translation t_matrixLineMin;
+extern Translation t_matrixLineMax;
+extern Translation t_matrixSpeedMin;
+extern Translation t_matrixSpeedMax;
+extern Translation t_matrixUpdateInterval;
+
+#endif // MATRIXTRANSLATIONS_H
