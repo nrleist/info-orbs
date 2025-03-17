0e2f84ddd49b6c4b560feb5c865b39ad731b6e62
nconner-06 <neil.conner@outlook.com>
2025-03-17 19:11:19 +0100

    Update GlobalTime.cpp

--- firmware/src/core/globaltime/GlobalTime.cpp
+++ firmware/src/core/globaltime/GlobalTime.cpp
@@ -196,3 +196,7 @@ bool GlobalTime::setFormat24Hour(bool format24hour) {
     m_format24hour = format24hour;
     return m_format24hour;
 }
+
+int GlobalTime::getTimeZoneOffset() {
+    return m_timeZoneOffset;
+}
